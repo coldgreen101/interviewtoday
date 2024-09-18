48bf58d09ce85d825fac590404690a74d8749ac6
coldgreen101 <domjangrkinic@gmail.com>
2024-09-18 15:28:21 +0200

    Adjust stat card opacity and add text content

--- src/app/components/Stats33.tsx
+++ src/app/components/Stats33.tsx
@@ -1,7 +1,8 @@
 type StatCard = {
   percentage: string;
   title: string;
-  description: string;
+  description1: string;
+  description2: string;
 };
 
 type Stat = {
@@ -42,7 +43,7 @@ export const Stats33 = (props: Stats33Props) => {
 const Stat = (stat: Stat) => {
   return (
     <div className="grid grid-cols-3 gap-x-6 gap-y-7 sm:gap-x-6 sm:gap-y-6 lg:gap-x-8 lg:gap-y-8">
-      <div className="flex w-full flex-col justify-between bg-cardBackground bg-opacity-50 text-altBackground p-8 rounded-2xl">
+      <div className="flex w-full flex-col justify-between bg-[#4dc9f07a] bg-opacity-50 text-altBackground p-8 rounded-2xl">
         <p className="mb-5 text-6xl font-bold md:mb-6 md:text-9xl lg:text-10xl">
           {stat.statCards[0].percentage}
         </p>
@@ -50,10 +51,11 @@ const Stat = (stat: Stat) => {
           <h3 className="text-md font-bold leading-[1.4] md:text-xl">
             {stat.statCards[0].title}
           </h3>
-          <p className="mt-2 mb-3">{stat.statCards[0].description}</p>
+          <p className="mt-2 mb-3">{stat.statCards[0].description1}</p>
+          <p className="mt-2">{stat.statCards[0].description2}</p>
         </div>
       </div>
-      <div className="flex w-full flex-col justify-between bg-altBackground text-cardText p-8 rounded-2xl">
+      <div className="flex w-full flex-col justify-between bg-[#3a0ba3d9] text-cardText p-8 rounded-2xl">
         <p className="mb-5 text-6xl font-bold md:mb-6 md:text-9xl lg:text-10xl">
           {stat.statCards[1].percentage}
         </p>
@@ -61,10 +63,11 @@ const Stat = (stat: Stat) => {
           <h3 className="text-md font-bold leading-[1.4] md:text-xl">
             {stat.statCards[1].title}
           </h3>
-          <p className="mt-2 mb-3">{stat.statCards[1].description}</p>
+          <p className="mt-2 mb-3">{stat.statCards[1].description1}</p>
+          <p className="mt-2">{stat.statCards[1].description2}</p>
         </div>
       </div>
-      <div className="flex w-full flex-col justify-between bg-primary bg-opacity-75 text-altBackground p-8 rounded-2xl">
+      <div className="flex w-full flex-col justify-between bg-[#f7248599] text-altBackground p-8 rounded-2xl">
         <p className="mb-5 text-6xl font-bold md:mb-6 md:text-9xl lg:text-10xl">
           {stat.statCards[2].percentage}
         </p>
@@ -72,7 +75,8 @@ const Stat = (stat: Stat) => {
           <h3 className="text-md font-bold leading-[1.4] md:text-xl">
             {stat.statCards[2].title}
           </h3>
-          <p className="mt-2 mb-3">{stat.statCards[2].description}</p>
+          <p className="mt-2 mb-3">{stat.statCards[2].description1}</p>
+          <p className="mt-2">{stat.statCards[2].description2}</p>
         </div>
       </div>
     </div>
@@ -87,18 +91,27 @@ export const Stats33Defaults: Stats33Props = {
     statCards: [
       {
         percentage: "30%",
-        title: "Short heading goes here",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "DHW & Company",
+        description1:
+          "Impact: 34% increase in staffing levels and 8% boost in guest satisfaction within two months.",
+        description2:
+          "Benefit: Streamlined hiring processes allowing managers to focus on customer service rather than recruitment.",
       },
       {
         percentage: "30%",
-        title: "Short heading goes here",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "Gellert Hospitality Group",
+        description1:
+          "Impact: Staffing levels rose by 41%, with sales growing 25.4% in the first year.",
+        description2:
+          "Benefit: Enabled aggressive expansion and efficient staffing of new locations, supporting business growth.",
       },
       {
         percentage: "30%",
-        title: "Short heading goes here",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "NYVA Restaurant Group",
+        description1:
+          "Impact: Achieved proper staffing in 90% of locations, enhancing operational efficiency and sales.",
+        description2:
+          "Benefit: Simplified recruitment process led to significant time savings and higher management satisfaction.",
       },
     ],
   },
