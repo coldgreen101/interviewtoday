278e7f11971055168936f0cc9d343df278326e36
coldgreen101 <domjangrkinic@gmail.com>
2024-09-19 09:12:01 +0200

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
-import { Navbar14 as Navbar } from "./components/Navbar"
-import { Header26 as Header } from "./components/Header"
-import { Logo3 as Logo } from "./components/Logo"
-import { Layout242 as Process } from "./components/Layout242"
-import { Layout27 as Benefits }  from "./components/Layout27"
-import { Stats33 as Stats } from "./components/Stats33"
-import { Layout253 as Features } from "./components/Layout253"
-import { Testimonial1 as Testimonial } from "./components/Testimonial"
-import { Cta57 as Cta } from "./components/Cta"
-import { Footer4 as Footer } from "./components/Footer"
-
+import { Navbar14 as Navbar } from "./components/Navbar";
+import { Header26 as Header } from "./components/Header";
+import { Logo3 as Logo } from "./components/Logo";
+import { Layout242 as Process } from "./components/Layout242";
+import { Layout27 as Benefits } from "./components/Layout27";
+import { Stats33 as Stats } from "./components/Stats33";
+import { Layout253 as Features } from "./components/Layout253";
+import { Testimonial1 as Testimonial } from "./components/Testimonial";
+import { Cta57 as Cta } from "./components/Cta";
+import { Footer4 as Footer } from "./components/Footer";
 
 export default function Home() {
   return (
